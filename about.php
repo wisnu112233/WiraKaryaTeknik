f0bcc1c12dd4473a360ff4c139de2bf2e22e94d2
wisnu112233 <wisnuafif7@gmail.com>
2024-07-24 22:45:21 +0700

    Add files via upload

---
+++ about.php
@@ -0,0 +1,194 @@
+<?php
+include('./includes/connect.php');
+include('./functions/common_functions.php');
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wira Karya Teknik</title>
+    <link rel="stylesheet" href="./assets/css/bootstrap.css" />
+    <link rel="stylesheet" href="./assets/css/main.css" />
+</head>
+
+<body>
+    <!-- upper-nav -->
+    <div class="upper-nav primary-bg p-2 px-3 text-center text-break">
+        <span>Promo Terbatas Gajian Sale - Diskon 50%! <a>Shop Now</a></span>
+    </div>
+    <!-- upper-nav -->
+    <!-- Start NavBar -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+            <a class="navbar-brand fw-bold" href="#">Wira Karya Teknik</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="./index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="./about.php">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="./products.php">Products</a>
+                    </li>
+                </ul>
+                <form class="d-flex">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-primary" type="submit">Search</button>
+                </form>
+                <ul class="navbar-nav mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" href="./cart.php"><svg width="28" height="28" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M11 27C11.5523 27 12 26.5523 12 26C12 25.4477 11.5523 25 11 25C10.4477 25 10 25.4477 10 26C10 26.5523 10.4477 27 11 27Z" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                                <path d="M25 27C25.5523 27 26 26.5523 26 26C26 25.4477 25.5523 25 25 25C24.4477 25 24 25.4477 24 26C24 26.5523 24.4477 27 25 27Z" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                                <path d="M3 5H7L10 22H26" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                                <path d="M10 16.6667H25.59C25.7056 16.6667 25.8177 16.6267 25.9072 16.5535C25.9966 16.4802 26.0579 16.3782 26.0806 16.2648L27.8806 7.26479C27.8951 7.19222 27.8934 7.11733 27.8755 7.04552C27.8575 6.97371 27.8239 6.90678 27.7769 6.84956C27.73 6.79234 27.6709 6.74625 27.604 6.71462C27.5371 6.68299 27.464 6.66661 27.39 6.66666H8" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                            </svg>
+                            <sup>
+                                <?php
+                                cart_item();
+                                ?>
+                            </sup>
+                            <span class="d-none">
+                                Total Price is: 
+                                <?php
+                                total_cart_price();
+                                ?>
+                            </span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" class="d-flex align-items-center gap-1" href="#">
+                            <svg width="28" height="28" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M24 27V24.3333C24 22.9188 23.5224 21.5623 22.6722 20.5621C21.8221 19.5619 20.669 19 19.4667 19H11.5333C10.331 19 9.17795 19.5619 8.32778 20.5621C7.47762 21.5623 7 22.9188 7 24.3333V27" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                                <path d="M16.5 14C18.9853 14 21 11.9853 21 9.5C21 7.01472 18.9853 5 16.5 5C14.0147 5 12 7.01472 12 9.5C12 11.9853 14.0147 14 16.5 14Z" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                            </svg>
+                            <?php
+                                if(!isset($_SESSION['username'])){
+                                    echo "<span>
+                                    Welcome guest
+                                </span>";
+                            }else{
+                                    echo "<span>
+                                    Welcome ".$_SESSION['username']. "</span>";
+                                }
+                                ?>
+                        </a>
+                    </li>
+                    <?php
+                        if(isset($_SESSION['username'])){                            
+                            echo "
+                            <li class='nav-item'>
+                            <a class='nav-link' href='./users_area/profile.php'>My Account</a>
+                        </li>";
+                        }
+                        else{
+                            echo "
+                            <li class='nav-item'>
+                            <a class='nav-link' href='./users_area/user_registration.php'>Register</a>
+                        </li>";
+                        }
+                    ?>
+                    <?php
+                    if(!isset($_SESSION['username'])){
+                        echo "<li class='nav-item'>
+                        <a class='nav-link' href='./users_area/user_login.php'>
+                            Login
+                        </a>
+                    </li>";
+                }else{
+                        echo "<li class='nav-item'>
+                        <a class='nav-link' href='./users_area/logout.php'>
+                            Logout
+                        </a>
+                    </li>";
+                    }
+                    ?>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- End NavBar -->
+
+    <!-- Start All Prodcuts  -->
+<style>
+    .banner .img {
+        width: 100%;
+        height: 250px;
+        background-image: url('assets/image/apa.jpeg');
+        padding: 0px;
+        margin: 0px;
+    }
+
+    .img .box {
+        height: 250px;
+        background-color: rgb(41, 41, 41, 0.7);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        color: white;
+        padding-top: 70px;
+    }
+
+    .box a {
+        color: #0066FF;
+    }
+
+    .box a:hover {
+        text-decoration: none;
+        color: rgb(6, 87, 209);
+    }
+
+    .row {
+        margin-left: 0;
+        margin-right: 0;
+    }
+
+    .row>[class^="col-sm-4"] {
+        padding-left: 0;
+        padding-right: 0;
+    }
+
+    .row>[class^="col-sm-8"] {
+        padding-right: 100px;
+    }
+</style>
+
+<div class="banner mb-3">
+    <div class="container-fluid img">
+        <div class="container-fluid box">
+            <h3>ABOUT US</h3>
+            <p>Home > <a href="index.php">Beranda</a></p>
+        </div>
+    </div>
+</div>
+
+<div class="container bg-white rounded pt-4 pb-4">
+    <div class="row no-gutter">
+        <div class="col-sm-8 text-justify">
+            <h4>ABOUT US WIRA KARYA TEKNIK</h4><br>
+            <p>Wira Karya Teknik adalah sebuah wirausaha yang bergerak dibagian jual beli sparepart barang elektronik yang berdiri sejak akhir 2023.</p>
+            <p> Visi kami adalah menjadikan kualitas barang yang kita jual memiliki nomor satu dari segi apapun
+            karena kepuasan pelanggan belanja dikami adalah prioritas utama untuk kami.</p>
+            <P>KAMI HANYA MELAYANI PEMBAYARAN MENGGUNAKAN ONLINE BERUPA MANDIRI,BRI dan BTN</P>
+            <P>JIKA INGIN MENGGUNAKAN COD BELUM TERSEDIA NAMUN UNTUK WILAYAH BEKASI DAN SEKITARNYA LANGSUNG SAJA KE TOKO</P>
+
+        </div>
+        <div class="col-sm-4">
+            <img src="assets/images/logo11.jpeg" height="200px" width="100%">
+        </div>
+        
+    </div>
+</div>
+</script>
+<?php require "footer.php"; ?>
+
+</html>
